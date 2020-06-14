a0fe80f9d92799d4718ffafaf38ed5bc6084737f
= <=>
2020-06-14 12:18:53 +0200

    Exception class

---
+++ Bank_Nawratil/src/domain/BankAccountException.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class BankAccountException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public BankAccountException(String message) {
+		super(message);
+	}
+}
